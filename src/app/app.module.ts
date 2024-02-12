4f919bec212eee394d87a4b776dd3628d62ea204
Miguel Correa <macc1712@gmail.com>
2024-02-12 14:19:07 -0500

    Fin sección 3: Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
@@ -11,6 +11,9 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +24,14 @@ import { SideBarComponent } from './shared/side-bar/side-bar.component';
     DashboardComponent,
     BreadcrumbsComponent,
     HeaderComponent,
-    SideBarComponent
+    SideBarComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/no-page-found/no-page-found.component.ts
+++ src/app/pages/no-page-found/no-page-found.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-no-page-found',
   templateUrl: './no-page-found.component.html',
-  styles: ``
+  styleUrl: './no-page-found.component.css',
 })
 export class NoPageFoundComponent {
+
+  year = new Date().getFullYear()
 
 }
